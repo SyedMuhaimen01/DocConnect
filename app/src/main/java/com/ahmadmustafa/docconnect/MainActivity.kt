7b4b762e6ad44b55121e26d8c80b62304a5eda0a
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-05-06 21:18:48 +0500

    App UI: splash Screen Login sign up options Sign Up as Medic Sign up as Patient Sign up as medical centre user profiles edit profiles set working hours cancel appointment Payment summary

--- app/src/main/java/com/ahmadmustafa/docconnect/MainActivity.kt
+++ app/src/main/java/com/ahmadmustafa/docconnect/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_feedback)
+        setContentView(R.layout.activity_select_appointment_date)
 
         //button for image view
 
