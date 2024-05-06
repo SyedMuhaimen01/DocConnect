543d8294baac1be17953ff05644443578b1e3b88
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-05-06 21:31:03 +0500

    App UI:

---
+++ app/src/main/java/com/ahmadmustafa/docconnect/login.kt
@@ -0,0 +1,23 @@
+package com.ahmadmustafa.docconnect
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+
+class login : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "WrongViewCast")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        //button listener for signup btn
+        val signup = findViewById<TextView>(R.id.signup)
+        signup.setOnClickListener {
+
+            val intent = Intent(this, signupPatient::class.java)
+            startActivity(intent)
+        }
+    }
+}
