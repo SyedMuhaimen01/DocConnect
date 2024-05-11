7a9464f6ec79f9beb5016a6545935a2da981625a
SyedMuhaimen01 <i210888@nu.edu.pk>
2024-05-11 15:39:08 +0500

    Signup for 3 interfaces

--- app/src/main/java/com/ahmadmustafa/docconnect/signupCenter.kt
+++ app/src/main/java/com/ahmadmustafa/docconnect/signupCenter.kt
@@ -1,13 +1,37 @@
 package com.ahmadmustafa.docconnect
-
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+
+data class Center(
+    var id: String = "",
+    var name: String = "",
+    var email: String = "",
+    var contactNumber: String = "",
+    var address: String = "",
+    var category: String = "",
+    var password: String = "",
+    var centerStatus: Boolean = false,
+    val picture: String? = ""
+)
 
 class signupCenter : AppCompatActivity() {
+    private val NOTIFICATION_CHANNEL_ID = "RegistrationNotification"
     private lateinit var centernameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var contactEditText: EditText
@@ -16,10 +40,13 @@ class signupCenter : AppCompatActivity() {
     private lateinit var passwordEditText: EditText
     private lateinit var signupButton: Button
     private lateinit var logTextView: TextView
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_center)
+
+        auth = FirebaseAuth.getInstance()
 
         centernameEditText = findViewById(R.id.centername)
         emailEditText = findViewById(R.id.email)
@@ -39,18 +66,46 @@ class signupCenter : AppCompatActivity() {
             val password = passwordEditText.text.toString().trim()
 
             if (validateInput(centername, email, contact, address, type, password)) {
-                val intent = Intent(this, adminHome::class.java)
-                startActivity(intent)
+                checkCenterExists(centername, address) { centerExists ->
+                    if (!centerExists) {
+                        // Register center with Firebase Authentication
+                        auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    // Registration successful, save center details to Firebase database
+                                    val user = auth.currentUser
+                                    val center = Center(
+                                        user?.uid ?: "",
+                                        centername,
+                                        email,
+                                        contact,
+                                        address,
+                                        type,
+                                        password,
+                                        centerStatus = false
+                                    )
+                                    saveCenterToFirebase(center)
+                                    saveCenterToSharedPreferences(center)
+                                    showRegistrationSuccessNotification()
+                                    val intent = Intent(this, adminHome::class.java)
+                                    startActivity(intent)
+                                } else {
+                                    // Registration failed
+                                    showToast("Registration Failed. Failed to register center. Please try again later.")
+                                }
+                            }
+                    } else {
+                        showToast("Registration Failed Center already exists.")
+                    }
+                }
             }
         }
 
         logTextView.setOnClickListener {
-            // Handle click on "Already have an account? Sign In" text
             val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
     }
-
     private fun validateInput(centername: String, email: String, contact: String, address: String,
                               type: String, password: String): Boolean {
         if (centername.isEmpty()) {
@@ -93,5 +148,96 @@ class signupCenter : AppCompatActivity() {
         }
 
         return true
+    }
+
+    private fun checkCenterExists(name: String, address: String, callback: (Boolean) -> Unit) {
+        val database = FirebaseDatabase.getInstance()
+        val reference = database.getReference("centers")
+
+        reference.orderByChild("name").equalTo(name)
+            .addListenerForSingleValueEvent(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (snapshot in dataSnapshot.children) {
+                        val center = snapshot.getValue(Center::class.java)
+                        if (center != null && center.address == address) {
+                            // Center with same name and address found
+                            callback.invoke(true)
+                            return
+                        }
+                    }
+                    // No center found with the same name and address
+                    callback.invoke(false)
+                }
+
+                override fun onCancelled(databaseError: DatabaseError) {
+                    // Error occurred while fetching data
+                    showToast("Error. Failed to check center existence: ${databaseError.message}")
+                    callback.invoke(false)
+                }
+            })
+    }
+
+
+    private fun saveCenterToFirebase(center: Center) {
+        val database = FirebaseDatabase.getInstance()
+        val reference = database.getReference("centers")
+
+        reference.child(center.id).setValue(center)
+            .addOnSuccessListener {
+                // Call saveCenter to save details to SharedPreferences after Firebase save is successful
+            }
+            .addOnFailureListener { e ->
+                showToast("Error. Failed to save center: ${e.message}")
+            }
+    }
+
+    private fun saveCenterToSharedPreferences(center: Center) {
+        // Save center details to Firebase
+        saveCenterToFirebase(center)
+
+        // Save center details to SharedPreferences
+        val sharedPreferences = getSharedPreferences("CenterPreferences", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        editor.putString("centerId", center.id)
+        editor.putString("centerName", center.name)
+        editor.putString("centerEmail", center.email)
+        editor.putString("centerContact", center.contactNumber)
+        editor.putString("centerAddress", center.address)
+        editor.putString("centerCategory", center.category)
+        editor.putString("centerPassword", center.password)
+        editor.putBoolean("centerStatus", center.centerStatus)
+        editor.putString("centerPicture", center.picture)
+        editor.apply()
+    }
+
+
+
+    private fun showRegistrationSuccessNotification() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                NOTIFICATION_CHANNEL_ID,
+                "Registration Notification",
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                description = "Notification for registration success"
+                enableLights(true)
+                lightColor = Color.GREEN
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.notifications_icon_foreground)
+            .setContentTitle("Account Registeration Status")
+            .setContentText("Your account has been successfully registered.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        notificationManager.notify(1, builder.build())
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
--- app/src/main/java/com/ahmadmustafa/docconnect/signupPatient.kt
+++ app/src/main/java/com/ahmadmustafa/docconnect/signupPatient.kt
@@ -1,25 +1,52 @@
 package com.ahmadmustafa.docconnect
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.*
 
+data class Patient(
+    var id: String = "",
+    var name: String = "",
+    var email: String = "",
+    var contactNumber: String = "",
+    var cnic: String = "",
+    var password: String = "",
+    val picture: String? = ""
+)
 
 class signupPatient : AppCompatActivity() {
+    private val NOTIFICATION_CHANNEL_ID = "RegistrationNotification"
     private lateinit var usernameEditText: EditText
     private lateinit var emailEditText: EditText
     private lateinit var contactEditText: EditText
     private lateinit var cnicEditText: EditText
     private lateinit var passwordEditText: EditText
     private lateinit var signupButton: Button
-    private lateinit var logTextView: TextView // Corrected type to TextView
+    private lateinit var logTextView: TextView
+    private lateinit var database: DatabaseReference
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup_patient)
+
+        auth = FirebaseAuth.getInstance()
+        database = FirebaseDatabase.getInstance().reference.child("patients")
+        sharedPreferences = getSharedPreferences("patients", Context.MODE_PRIVATE)
 
         usernameEditText = findViewById(R.id.username)
         emailEditText = findViewById(R.id.email)
@@ -27,7 +54,7 @@ class signupPatient : AppCompatActivity() {
         cnicEditText = findViewById(R.id.cnic)
         passwordEditText = findViewById(R.id.password)
         signupButton = findViewById(R.id.signup)
-        logTextView = findViewById(R.id.log) // Corrected type to TextView
+        logTextView = findViewById(R.id.log)
 
         signupButton.setOnClickListener {
             val username = usernameEditText.text.toString().trim()
@@ -37,15 +64,88 @@ class signupPatient : AppCompatActivity() {
             val password = passwordEditText.text.toString().trim()
 
             if (validateInput(username, email, contact, cnic, password)) {
-                val intent = Intent(this, Home::class.java)
-                startActivity(intent)
+                // Check if CNIC already exists
+                checkCNICExistsInPatients(cnic) { cnicExists ->
+                    if (!cnicExists) {
+                        // Create user with email and password
+                        auth.createUserWithEmailAndPassword(email, password)
+                            .addOnCompleteListener(this) { task ->
+                                if (task.isSuccessful) {
+                                    val firebaseUser = auth.currentUser
+                                    val userId = firebaseUser?.uid ?: ""
+
+                                    // Store the patient details in the Realtime Database
+                                    val patient = Patient(
+                                        id = userId,
+                                        name = username,
+                                        email = email,
+                                        contactNumber = contact,
+                                        cnic = cnic,
+                                        password = password
+                                    )
+                                    savePatientToFirebase(patient)
+                                    savePatientToSharedPreferences(patient)
+                                    showRegistrationSuccessNotification()
+                                    // Start Home activity
+                                    val intent = Intent(this, Home::class.java)
+                                    startActivity(intent)
+                                } else {
+                                    if (task.exception?.message?.contains("email address is already in use") == true) {
+                                        Toast.makeText(
+                                            baseContext, "Email already in use.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    } else {
+                                        Toast.makeText(
+                                            baseContext, "Authentication failed.",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }
+                    } else {
+                        Toast.makeText(
+                            baseContext, "CNIC already exists.",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    }
+                }
             }
         }
 
-        logTextView.setOnClickListener { // Using logTextView
+        logTextView.setOnClickListener {
             val intent = Intent(this, login::class.java)
             startActivity(intent)
         }
+    }
+
+    private fun checkCNICExistsInPatients(cnic: String, callback: (Boolean) -> Unit) {
+        val query: Query = database.orderByChild("cnic").equalTo(cnic)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                callback(dataSnapshot.exists())
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+                callback(false)
+            }
+        })
+    }
+
+    private fun savePatientToFirebase(patient: Patient) {
+        database.child(patient.id).setValue(patient)
+    }
+
+    private fun savePatientToSharedPreferences(patient: Patient) {
+        val editor = sharedPreferences.edit()
+        editor.putString("id", patient.id)
+        editor.putString("name", patient.name)
+        editor.putString("email", patient.email)
+        editor.putString("contactNumber", patient.contactNumber)
+        editor.putString("cnic", patient.cnic)
+        editor.putString("password", patient.password)
+        editor.putString("picture", patient.picture)
+        editor.apply()
     }
 
     private fun validateInput(username: String, email: String, contact: String, cnic: String, password: String): Boolean {
@@ -84,5 +184,25 @@ class signupPatient : AppCompatActivity() {
         }
 
         return true
+    }
+    private fun showRegistrationSuccessNotification() {
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(NOTIFICATION_CHANNEL_ID, "Registration Notification", NotificationManager.IMPORTANCE_DEFAULT).apply {
+                description = "Notification for registration success"
+                enableLights(true)
+                lightColor = Color.GREEN
+            }
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
+            .setSmallIcon(R.drawable.notifications_icon_foreground)
+            .setContentTitle("Account Registeration Status")
+            .setContentText("Your account has been successfully registered.")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        notificationManager.notify(1, builder.build())
     }
 }
