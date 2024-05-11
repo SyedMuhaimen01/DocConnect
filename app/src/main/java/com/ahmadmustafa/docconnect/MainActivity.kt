0c922353deabc3ad1e048153fc9f1448593b61ee
SyedMustafa20 <samustafa290@gmail.com>
2024-05-11 14:01:03 +0500

    Updated Pages traversal

--- app/src/main/java/com/ahmadmustafa/docconnect/MainActivity.kt
+++ app/src/main/java/com/ahmadmustafa/docconnect/MainActivity.kt
@@ -1,23 +1,33 @@
 package com.ahmadmustafa.docconnect
 
-import android.annotation.SuppressLint
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val swipeButton=findViewById<ImageView>(R.id.swipe)
+        val swipeButton = findViewById<ImageView>(R.id.swipe)
         swipeButton.setOnClickListener {
-            val intent = Intent(this, login::class.java)
-            startActivity(intent)
+            showLoginDialog()
         }
     }
 
+    private fun showLoginDialog() {
+        MaterialAlertDialogBuilder(this)
+            .setTitle("Choose an option:")
+            .setPositiveButton("Continue as Guest") { dialog, which ->
+                val intent = Intent(this, Home::class.java)
+                startActivity(intent)
+            }
+            .setNegativeButton("Login") { dialog, which ->
+                val intent = Intent(this, login::class.java)
+                startActivity(intent)
+            }
+            .show()
+    }
 }
